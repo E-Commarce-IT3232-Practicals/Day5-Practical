e3a449e643bef84a36b43911a74bfd2582c4ede8
madurangaPrabhath <139701327+madurangaPrabhath@users.noreply.github.com>
2025-05-13 18:11:31 +0530

    Project file uploaded

---
+++ myapp5/src/main/java/lk/ac/vau/fas/ict/Myapp5Application.java
@@ -0,0 +1,13 @@
+package lk.ac.vau.fas.ict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myapp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myapp5Application.class, args);
+	}
+
+}
